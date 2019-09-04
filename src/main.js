c06138dfa50f5d4ed5b0317de7ff68e7f7e175e7
Jordan Safford and Zach Krogh <zach@>
2019-09-04 15:16:13 -0700

    display every number less than a 1000 that is ls a multiple of 3 or 5

---
+++ src/main.js
@@ -0,0 +1,19 @@
+
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+
+for (var i = 1; i <= 1000; i++) {
+  if (i % 3 === 0) {
+    console.log(i)
+}
+  else if (i % 5 === 0) {
+      console.log(i)
+  }
+}
+
+$(document).ready(function(){
+
+});
